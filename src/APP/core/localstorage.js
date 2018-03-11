a81d3ef5fc9544683724d70ba6f1a900d74a1127
Claudia Reynders <exotic.maui@gmail.com>
2018-03-11 18:22:18 +0100

    Added localstorage

---
+++ src/APP/core/localstorage.js
@@ -0,0 +1,31 @@
+let localStorage = window.localStorage;
+
+export function loadState() {
+
+  try {
+    const serializedState = localStorage.getItem('state');
+
+    if(serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+      console.error(err);
+      return undefined;
+  }
+}
+
+export function saveState(state) {
+
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (err) {
+      console.error(err);
+  }
+}
+
+
+export function clearState() {
+  localStorage.clear();
+}
